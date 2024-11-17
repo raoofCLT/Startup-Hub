0a885997fa0c38aed980f8397b5f6fc821aa2e62
Abdul Raoof <raoofkottayil@gmail.com>
2024-11-17 18:47:53 +0530

    feat(page) create user page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import {auth, signOut, signIn} from "@/auth"
+import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
 
 const Navbar = async () => {
-  const session = await auth()
+  const session = await auth();
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
@@ -15,28 +16,42 @@ const Navbar = async () => {
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
-            <Link href={"/startup/create"}>
-            <span>Create</span>
-            </Link>
-            <form action={async () => {
-              "use server";
+              <Link href={"/startup/create"}>
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
 
-              await signOut({redirectTo: "/"})
-            }}>
-              <button type="submit">Logout</button>
-            </form>
-            <Link href={`user/${session?.id}`}>
-            <span>{session?.user?.name}</span>
-            </Link>
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
+              </form>
+              <Link href={`user/${session?.id}`}>
+                <Image
+                  src={session?.user?.image || "/default-profile.png"}
+                  alt={session?.user?.name || ""}
+                  width={35}
+                  height={35}
+                  className="rounded-full"
+                />
+              </Link>
             </>
-          ):(
-            <form action={async () => {
-              "use server";
+          ) : (
+            <form
+              action={async () => {
+                "use server";
 
-              await signIn('github')
-            }}>
+                await signIn("github");
+              }}
+            >
               <button type="submit">Login</button>
-             </form>
+            </form>
           )}
         </div>
       </nav>
